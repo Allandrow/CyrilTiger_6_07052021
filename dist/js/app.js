0ab7b05ab54c03009ce459d0685579ad601390d0
Cyril Tiger <cyriltiger@gmail.com>
2021-05-13 11:32:30 +0200

    WIP refactorisation JS

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,6 +1,3 @@
-// DOM Elements
-const navUl = document.getElementById('js-nav');
-
 const getJSON = async () => {
   const data = await fetch('dist/js/data/fisheyedata.json');
   const json = await data.json();
@@ -18,11 +15,7 @@ const getPhotographTagSet = (data) => {
   return tagSet;
 };
 
-const capitalize = (string) => {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-};
-
-const createItemListLinkElement = (textNode) => {
+const createDOMItemListLinkElement = (textNode) => {
   const li = document.createElement('li');
 
   const link = document.createElement('a');
@@ -40,7 +33,20 @@ const createItemListLinkElement = (textNode) => {
   return li;
 };
 
-const createThumbnailDOMElement = (imgUrl, name, localisationText, sloganText, priceNumber, tagsList) => {
+const generateDOMNavigationTagList = (data) => {
+  const navUl = document.getElementById('js-nav');
+  const photographTagSet = getPhotographTagSet(data);
+
+  for (const tag of photographTagSet) {
+    navUl.appendChild(createDOMItemListLinkElement(tag));
+  }
+};
+
+const capitalize = (string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+const createDOMThumbnailElement = (object) => {
   const thumbnail = 'thumbnail';
 
   const div = document.createElement('div');
@@ -51,11 +57,11 @@ const createThumbnailDOMElement = (imgUrl, name, localisationText, sloganText, p
   link.classList += `${thumbnail}__link`;
 
   const img = document.createElement('img');
-  img.setAttribute('src', imgUrl);
-  img.setAttribute('alt', name);
+  img.setAttribute('src', `dist/img/thumbnails/${object.portrait}`);
+  img.setAttribute('alt', object.name);
 
   const title = document.createElement('h2');
-  title.appendChild(document.createTextNode(name));
+  title.appendChild(document.createTextNode(object.name));
 
   link.append(img, title);
 
@@ -63,60 +69,44 @@ const createThumbnailDOMElement = (imgUrl, name, localisationText, sloganText, p
 
   const localisation = document.createElement('span');
   localisation.classList += `${thumbnail}__localisation`;
-  localisation.appendChild(document.createTextNode(localisationText));
+  localisation.appendChild(document.createTextNode(`${object.city}, ${object.country}`));
 
   const slogan = document.createElement('span');
   slogan.classList += `${thumbnail}__slogan`;
-  slogan.appendChild(document.createTextNode(sloganText));
+  slogan.appendChild(document.createTextNode(object.tagline));
 
   const price = document.createElement('span');
   price.classList += `${thumbnail}__price`;
-  price.appendChild(document.createTextNode(`${priceNumber}&euro;/jour`));
+  price.appendChild(document.createTextNode(`${object.price}&euro;/jour`));
 
   paragraph.append(localisation, slogan, price);
 
-  div.append(link, paragraph);
+  const ul = document.createElement('ul');
+  ul.classList += 'tagList';
+
+  object.tags.forEach((tag) => {
+    ul.appendChild(createDOMItemListLinkElement(tag));
+  });
+
+  div.append(link, paragraph, ul);
 
   return div;
 };
 
-const generateDOMTagList = (collection, listDOM) => {
-  for (const item of collection) {
-    listDOM.appendChild(createItemListLinkElement(item));
-  }
-};
-
-const generateDOMThumbnail = (object) => {
-  const thumbnailImgURL = `dist/img/thumbnails/${object.portrait}`;
-  const thumbnailName = object.name;
-  const thumbnailLocalisation = `${object.city}, ${object.country}`;
-  const thumbnailSlogan = object.tagline;
-  const thumbnailPrice = object.price;
-  const thumbnailTags = object.tags;
-
+const generateDOMThumbnailList = (photographers) => {
   const thumbnailList = document.getElementById('js-thumbnailList');
 
-  thumbnailList.appendChild(
-    createThumbnailDOMElement(
-      thumbnailImgURL,
-      thumbnailName,
-      thumbnailLocalisation,
-      thumbnailSlogan,
-      thumbnailPrice,
-      thumbnailTags
-    )
-  );
+  photographers.forEach((photograph) => {
+    thumbnailList.appendChild(createDOMThumbnailElement(photograph));
+  });
 };
 
 async function onLoad() {
   const json = await getJSON();
-  const photographTagSet = getPhotographTagSet(json);
 
-  generateDOMTagList(photographTagSet, navUl);
+  generateDOMNavigationTagList(json);
 
-  json.photographers.forEach((photograph) => {
-    generateDOMThumbnail(photograph);
-  });
+  generateDOMThumbnailList(json.photographers);
 }
 
 document.addEventListener('DOMContentLoaded', onLoad());
